c94132eeb6b0e281bfde8c3d5482cd71b77677f9
jean-luc <jlhervy56@gmail.com>
2019-09-19 13:51:28 +0200

    few modifications

--- parameters.py
+++ parameters.py
@@ -1,13 +1,13 @@
-HEIGHT = 118
+HEIGHT = 82
 WIDTH = 486
-nb_img = 50
+nb_img = 20000
 channels = 1
-epochs = 15
-generation_folder = "inference"
-batch_size = 32
+epochs = 50
+generation_folder = "dataset"
+batch_size = 16
 nb_classes = 10
 max_barcode_number = 9999999999
 barcode_type = 'code39'
 input_shape = (HEIGHT, WIDTH, channels)
-barcode_options = dict(write_text=False, module_width=0.2, module_height=8.0, quiet_zone=1.5, font_size=10, text_distance=0.0, background="white", foreground="black")
+barcode_options = dict(write_text=False, module_width=0.2, module_height=5.0, quiet_zone=1.5, font_size=10, text_distance=0.0, background="white", foreground="black")
 
